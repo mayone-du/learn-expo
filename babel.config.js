dab5986965e668c5a3ec4e939a13c3c857f3fec9
mayone-du <yuki.ishikawa.dev@gmail.com>
2022-10-01 23:10:22 +0900

    🎉

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
